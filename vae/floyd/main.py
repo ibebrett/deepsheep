3b224660c9ae233dfa85b6af05db6f589eec40b1
Brett Jurman <i.be.brett@gmail.com>
2019-07-15 00:01:03 -0400

    more stuff related to floyd, parameter tuning

--- vae/floyd/main.py
+++ vae/floyd/main.py
@@ -1,3 +1,7 @@
+import json
+
+from comet_ml import Experiment
+
 import argparse
 
 from tqdm import tqdm
@@ -14,49 +18,12 @@ from torchvision import datasets, transforms
 
 from torchvision.utils import save_image
 
+from models import MODELS
 
-class VAE(nn.Module):
-    '''Simple VAE network, based on the example VAE found in
-    the pytorch examples.
-    '''
-
-    def __init__(self, image_w=200, image_h=200, latent_size=30):
-        super(VAE, self).__init__()
-
-        self._image_w = image_w
-        self._image_h = image_h
-        self._latent_size = latent_size
-
-        # Encoder layer into latent space.
-        self._encoder = nn.Sequential(
-            nn.Linear(3 * self._image_w * self._image_h, 400), nn.ReLU())
-
-        # Latent space to prob space
-        self._mean = nn.Linear(400, self._latent_size)
-        self._std = nn.Linear(400, self._latent_size)
-
-        # Latent space to decoded space.
-        self._decoder = nn.Sequential(
-            nn.Linear(self._latent_size, 400), nn.ReLU(),
-            nn.Linear(400, 3 * self._image_w * self._image_h), nn.Sigmoid())
-
-    def encode(self, x):
-        h1 = self._encoder(x)
-        return self._mean(h1), self._std(h1)
-
-    def reparameterize(self, mean, logvar):
-        std = torch.exp(0.5 * logvar)
-        eps = torch.randn_like(std)
-        return mean + eps * std
-
-    def decode(self, z):
-        return self._decoder(z)
-
-    def forward(self, x):
-        mean, logvar = self.encode(
-            x.view(-1, 3 * self._image_w * self._image_h))
-        z = self.reparameterize(mean, logvar)
-        return self.decode(z), mean, logvar, z
+# Create an experiment
+experiment = Experiment(api_key="LG4ZsUgJfz7Vhhsd271Zce9Dl",
+                        project_name="deepsheep",
+                        workspace="ibebrett")
 
 
 def get_data_loaders(path,
@@ -108,25 +75,21 @@ def get_data_loaders(path,
 # Some modifications:
 # 1. Clamped the inputs to binary_cross_entropy in attempt to avoid a bug.
 def loss_function(recon_x, x, mean, logvar, kld_weight):
-    BCE = F.binary_cross_entropy(torch.clamp(recon_x, 0.000001, 0.999999),
-                                 torch.clamp(x.view(-1, 3 * 200 * 200),
-                                             0.000001, 0.999999),
-                                 reduction='sum')
-
+    MSE = F.mse_loss(x, recon_x.view(-1, 3, 200, 200), reduction='sum') 
     # see Appendix B from VAE paper:
     # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
     # https://arxiv.org/abs/1312.6114
     # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
     KLD = -0.5 * torch.sum(1 + logvar - mean.pow(2) - logvar.exp())
 
-    return BCE, KLD, BCE + (KLD * kld_weight)
+    return MSE, KLD, MSE + (KLD * kld_weight)
 
 
 def train(epoch, device, model, optimizer, train_loader, kld_weight):
     model.train()
 
     total_loss = 0
-    total_bce_loss = 0
+    total_mse_loss = 0
     total_kld_loss = 0
     for i, (data, _) in tqdm(enumerate(train_loader)):
         optimizer.zero_grad()
@@ -134,45 +97,45 @@ def train(epoch, device, model, optimizer, train_loader, kld_weight):
 
         y, mean, logvar, z = model(X)
 
-        bce_loss, kld_loss, loss = loss_function(y, X, mean, logvar,
+        mse_loss, kld_loss, loss = loss_function(y, X, mean, logvar,
                                                  kld_weight)
         loss.backward()
         optimizer.step()
-        bce_loss = bce_loss.detach().cpu().numpy()
+        mse_loss = mse_loss.detach().cpu().numpy()
         kld_loss = kld_loss.detach().cpu().numpy()
         loss = loss.detach().cpu().numpy()
 
         total_loss += loss
-        total_bce_loss += bce_loss
+        total_mse_loss += mse_loss
         total_kld_loss += kld_loss
 
         if (i / len(train_loader)) > 0.2:
             # leave early due to weird problems. shuffling helps, save more.
             break
 
-    print(
-        json.dumps({
-            'epoch': epoch,
-            'metric': 'train_loss',
-            'value': total_loss
-        }))
-    print(
-        json.dumps({
-            'epoch': epoch,
-            'metric': 'train_kld_loss',
-            'value': total_kld_loss
-        }))
-    print(
-        json.dumps({
-            'epoch': epoch,
-            'metric': 'train_bce_loss',
-            'value': total_bce_loss
-        }))
+    #print(
+    #    json.dumps({
+    #        'epoch': epoch,
+    #        'metric': 'train_loss',
+    #        'value': total_loss
+    #    }))
+    #print(
+    #    json.dumps({
+    #        'epoch': epoch,
+    #        'metric': 'train_kld_loss',
+    #        'value': total_kld_loss
+    #    }))
+    #print(
+    #    json.dumps({
+    #        'epoch': epoch,
+    #        'metric': 'train_mse_loss',
+    #        'value': total_mse_loss
+    #    }))
 
     return {
-        'loss': total_loss / len(train_loader),
-        'bce': total_bce_loss / len(train_loader),
-        'kld': total_kld_loss / len(train_loader)
+        'reported_loss': total_loss / len(train_loader),
+        'reported_mse': total_mse_loss / len(train_loader),
+        'reported_kld': total_kld_loss / len(train_loader)
     }
 
 
@@ -180,56 +143,57 @@ def test(epoch, device, model, test_loader, kld_weight):
     model.eval()
 
     total_loss = 0
-    total_bce_loss = 0
+    total_mse_loss = 0
     total_kld_loss = 0
     with torch.no_grad():
         for i, (data, _) in tqdm(enumerate(test_loader)):
             X = data.to(device)
             y, mean, logvar, z = model(X)
-            bce_loss, kld_loss, loss = loss_function(y, X, mean, logvar,
+            mse_loss, kld_loss, loss = loss_function(y, X, mean, logvar,
                                                      kld_weight)
 
-            bce_loss = bce_loss.detach().cpu().numpy()
+            mse_loss = mse_loss.detach().cpu().numpy()
             kld_loss = kld_loss.detach().cpu().numpy()
             loss = loss.detach().cpu().numpy()
 
             total_loss += loss
-            total_bce_loss += bce_loss
+            total_mse_loss += mse_loss
             total_kld_loss += kld_loss
 
             if i == 0:
                 # For the same first batch of validation, emit the images.
                 unstacked_y = y.view(X.shape).cpu()
                 stacked_image = torch.cat((X.cpu(), unstacked_y), dim=0)
+                image_path = f'validate{epoch:04}.png'
                 torchvision.utils.save_image(
-                    torchvision.utils.make_grid(stacked_image),
-                    f'validate{epoch:04}.png')
+                    torchvision.utils.make_grid(stacked_image), image_path)
+                experiment.log_image(image_path, name=image_path)
 
             if (i / len(test_loader)) > 0.2:
                 break
-    print(
-        json.dumps({
-            'epoch': epoch,
-            'metric': 'test_loss',
-            'value': total_loss
-        }))
-    print(
-        json.dumps({
-            'epoch': epoch,
-            'metric': 'test_kld_loss',
-            'value': total_kld_loss
-        }))
-    print(
-        json.dumps({
-            'epoch': epoch,
-            'metric': 'test_bce_loss',
-            'value': total_bce_loss
-        }))
+    #print(
+    #    json.dumps({
+    #        'epoch': epoch,
+    #        'metric': 'test_loss',
+    #        'value': total_loss
+    #    }))
+    #print(
+    #    json.dumps({
+    #        'epoch': epoch,
+    #        'metric': 'test_kld_loss',
+    #        'value': total_kld_loss
+    #    }))
+    #print(
+    #    json.dumps({
+    #        'epoch': epoch,
+    #        'metric': 'test_mse_loss',
+    #        'value': total_mse_loss
+    #    }))
 
     return {
-        'loss': total_loss / len(test_loader),
-        'bce': total_bce_loss / len(test_loader),
-        'kld': total_kld_loss / len(test_loader)
+        'reported_loss': total_loss / len(test_loader),
+        'reported_mse': total_mse_loss / len(test_loader),
+        'reported_kld': total_kld_loss / len(test_loader)
     }
 
 
@@ -254,20 +218,18 @@ def save_model(path, model, optimizer, epoch):
         }, path)
 
 
-import json
-
-
 def main():
     parser = argparse.ArgumentParser(description='Train a VAE of sheep')
     parser.add_argument('data')
     parser.add_argument('--small', type=bool, default=False)
     parser.add_argument('--model_path', type=str, default='model')
-    parser.add_argument('--kld-weight', type=float, default=5.0)
+    parser.add_argument('--kld-weight', type=float, default=1.0)
     parser.add_argument('--latent-size', type=int, default=10)
     parser.add_argument('--batch-size', type=int, default=128)
     parser.add_argument('--learning-rate', type=float, default=1e-3)
     parser.add_argument('--loader-processes', type=int, default=4)
     parser.add_argument('--epochs', type=int, default=5)
+    parser.add_argument('--model', type=str, default='simple')
 
     args = parser.parse_args()
 
@@ -275,12 +237,14 @@ def main():
     with open('args.json', 'w') as f:
         json.dump(vars(args), f)
 
+    experiment.log_parameters(vars(args))
+
     model_path = args.model_path
     data_path = args.data
 
     device = torch.device("cuda")
 
-    model = VAE(latent_size=args.latent_size).to(device)
+    model = MODELS[args.model](latent_size=args.latent_size).to(device)
 
     optimizer = optim.Adam(model.parameters(), lr=args.learning_rate)
 
@@ -300,14 +264,20 @@ def main():
 
         epoch += 1
         epochs.append(epoch)
-        metrics['train'].append(
-            train(epoch, device, model, optimizer, train_data_loader,
-                  args.kld_weight))
+        with experiment.train():
+            metrics['train'].append(
+                train(epoch, device, model, optimizer, train_data_loader,
+                      args.kld_weight))
+            print(metrics['train'][-1], epoch)
+
+            experiment.log_metrics(metrics['train'][-1], step=epoch)
 
         save_model(model_path, model, optimizer, epoch)
 
-        metrics['test'].append(
-            test(epoch, device, model, test_data_loader, args.kld_weight))
+        with experiment.test():
+            metrics['test'].append(
+                test(epoch, device, model, test_data_loader, args.kld_weight))
+            experiment.log_metrics(metrics['test'][-1], step=epoch)
 
     with open('metrics.json', 'w') as f:
         json.dump(metrics, f)
--- vae/floyd/search.py
+++ vae/floyd/search.py
@@ -5,12 +5,13 @@ import hyperopt
 from hyperopt import hp
 
 space = {
-    'kld-weight': hp.loguniform('kld-weight', 0.1, 10.0),
-    'latent-size': hp.quniform('latent-size', 10, 200, 1),
-    'batch-size': hp.quniform('batch-size', 4, 8, 1),
-    'learning-rate': hp.loguniform('learning-rate', np.log(1e-3), np.log(0.5))
+    'kld-weight': hp.loguniform('kld-weight', 0.1, 2.0),
+    'latent-size': hp.quniform('latent-size', 10, 400, 1),
+    'batch-size': hp.quniform('batch-size', 4, 6, 1),
+    'learning-rate': hp.loguniform('learning-rate', np.log(1e-4),
+                                   np.log(0.001)),
+    'model': hp.choice('model', ['simple', 'conv'])
 }
-
 
 for _ in range(10):
     sample = hyperopt.pyll.stochastic.sample(space)
@@ -19,6 +20,7 @@ for _ in range(10):
     sample['batch-size'] = int(np.power(2, sample['batch-size']))
     sample['epochs'] = 10
 
-    arg_command = ' '.join([f'--{key}={value}' for key, value in sample.items()])
+    arg_command = ' '.join(
+        [f'--{key}={value}' for key, value in sample.items()])
     command = f'floyd run --gpu --data ibebrett/datasets/deepsheep/1:/deepsheep --env=pytorch-1.0 "python main.py /deepsheep {arg_command}"'
     subprocess.call(command, shell=True)
